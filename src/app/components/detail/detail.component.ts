c9c2cc5b69b935a8152be1abbf81190c29ca268a
maurya20 <mukesh.ice17@gmail.com>
2021-10-30 18:59:07 +0530

    geting id from url params to get product by id

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.css'],
 })
 export class DetailComponent implements OnInit {
+  product: any;
+  constructor(
+    private productsService: ProductsService,
+    private route: ActivatedRoute
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getProduct(id: number): void {
+    this.productsService
+      .getProduct(id)
+      .subscribe((product) => (this.product = product));
   }
-
+  ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    this.getProduct(id);
+  }
 }
