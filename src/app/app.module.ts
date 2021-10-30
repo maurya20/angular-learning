4096bf6a3a7e937a7e9c100d896b275d14b52f52
maurya20 <mukesh.ice17@gmail.com>
2021-10-30 11:05:52 +0530

    products list view on home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,12 @@ import { ButtonComponent } from './components/button/button.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './components/home/home.component';
 import { ProductsService } from './services/products.service';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 @NgModule({
   declarations: [AppComponent, HeaderComponent, ButtonComponent, HomeComponent],
   imports: [
@@ -19,6 +23,10 @@ import { ProductsService } from './services/products.service';
     MatSliderModule,
     MatButtonModule,
     HttpClientModule,
+    MatCardModule,
+    MatGridListModule,
+    MatIconModule,
+    MatBadgeModule,
   ],
   providers: [ProductsService],
   bootstrap: [AppComponent],
